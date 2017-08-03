1578bf531ee3479af3d5d50e9b0ee03e480ea59d
Raul Gomez Acuna <rauliyohmc@gmail.com>
2017-08-03 14:24:11 +0200

    100% test coverage for createMaterialPalette

--- src/__tests__/createMaterialPalette.test.js
+++ src/__tests__/createMaterialPalette.test.js
@@ -13,7 +13,8 @@ import { NativeModules } from 'react-native';
 import resolveAssetSource
   from 'react-native/Libraries/Image/resolveAssetSource';
 import createPalette from '../createMaterialPalette';
-import { defaultLightSwatch } from '../constants/defaults';
+import validateCreatePalette from '../utils/validateCreatePalette';
+import { defaultLightSwatch, defaultOptions } from '../constants/defaults';
 
 describe('createMaterialPalette', () => {
   beforeEach(() => {
@@ -21,10 +22,28 @@ describe('createMaterialPalette', () => {
     resolveAssetSource.mockReset();
   });
 
-  it('should provide null for the profiles not available', () => {
+  it('should create palette with default options if no options are provided', () => {
     NativeModules.MaterialPalette.createMaterialPalette.mockImplementation(() =>
       Promise.resolve({
         vibrant: defaultLightSwatch,
+      }));
+    resolveAssetSource.mockImplementation(() => `file://asset.jpg`);
+
+    return createPalette(0).then(() => {
+      expect(
+        NativeModules.MaterialPalette.createMaterialPalette,
+      ).toHaveBeenCalledWith(`file://asset.jpg`, {
+        type: ['vibrant'],
+        region: defaultOptions.region,
+        maximumColorCount: defaultOptions.maximumColorCount,
+      });
+    });
+  });
+
+  it('should call all the proper methods and provide null for the profiles not available', () => {
+    NativeModules.MaterialPalette.createMaterialPalette.mockImplementation(() =>
+      Promise.resolve({
+        lightVibrant: defaultLightSwatch,
         darkMuted: {
           color: 'green',
           population: 20,
@@ -32,11 +51,23 @@ describe('createMaterialPalette', () => {
           titleTextColor: 'red',
         },
       }));
+    resolveAssetSource.mockImplementation(() => `file://asset.jpg`);
 
-    return createPalette({
-      image: 0,
+    return createPalette(0, {
+      type: ['lightVibrant', 'darkMuted'],
     }).then(palette => {
-      expect(palette.vibrant).toBeNull();
+      expect(validateCreatePalette).toHaveBeenCalledWith(0, {
+        type: ['lightVibrant', 'darkMuted'],
+      });
+      expect(resolveAssetSource).toHaveBeenCalledWith(0);
+      expect(
+        NativeModules.MaterialPalette.createMaterialPalette,
+      ).toHaveBeenCalledWith(`file://asset.jpg`, {
+        type: ['lightVibrant', 'darkMuted'],
+        region: defaultOptions.region,
+        maximumColorCount: defaultOptions.maximumColorCount,
+      });
+      expect(palette.lightVibrant).toBeNull();
       expect(palette.darkMuted).toEqual({
         color: 'green',
         population: 20,
