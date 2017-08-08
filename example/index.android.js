c4fdfd7fe6bf0e01510324185e3a95598d52d88e
Raul Gomez Acuna <rauliyohmc@gmail.com>
2017-08-09 01:37:37 +0200

    Basic image gallery example

--- example/index.android.js
+++ example/index.android.js
@@ -1,36 +1,99 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Sample Material Palette App
  * @flow
  */
 
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Image, FlatList } from 'react-native';
-
+import {
+  AppRegistry,
+  StyleSheet,
+  FlatList,
+  Image,
+  View,
+  Text,
+} from 'react-native';
 import { createMaterialPalette } from 'react-native-material-palette';
 
-export default class TestPalette extends Component {
+type State = {
+  data: Array<Object>,
+};
+
+export default class Example extends Component<void, void, State> {
   state = {
-    isLoading: true,
-    palette: null,
+    data: [],
   };
 
+  async componentDidMount() {
+    const searchTerms = [
+      'nature',
+      'water',
+      'landscape',
+      'music',
+      'flower',
+      'house',
+    ];
+    const randomImageUrls = (await Promise.all(
+      searchTerms.map(term =>
+        fetch(`https://source.unsplash.com/featured/?${term}`)),
+    )).map((blob, index) => ({ url: blob.url, key: searchTerms[index] }));
+
+    const palettes = await Promise.all(
+      randomImageUrls.map(({ url }) =>
+        createMaterialPalette({ uri: url }, { type: 'muted' })),
+    );
+
+    // eslint-disable-next-line
+    this.setState({
+      data: randomImageUrls.map((imageData, index) => ({
+        ...imageData,
+        palette: palettes[index],
+      })),
+    });
+  }
+
   render() {
-    
+    if (!this.state.data.length) return null;
+    return (
+      <FlatList
+        contentContainerStyle={styles.list}
+        data={this.state.data}
+        renderItem={({ item }) => (
+          <View style={{ flex: 1, margin: 5 }}>
+            <Image
+              source={{ uri: item.url }}
+              style={{
+                flex: 1,
+                minWidth: 170,
+                maxWidth: 223,
+                height: 150,
+              }}
+            />
+            <View
+              style={{
+                backgroundColor: item.palette.muted.color,
+                height: 50,
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <Text style={{ color: item.palette.muted.bodyTextColor }}>
+                {item.key.toUpperCase()}
+              </Text>
+            </View>
+          </View>
+        )}
+      />
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  list: {
     justifyContent: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  image: {
-    height: 200,
-    width: null,
-    resizeMode: 'cover',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
 });
 
-AppRegistry.registerComponent('TestPalette', () => TestPalette);
+AppRegistry.registerComponent('TestPalette', () => Example);
