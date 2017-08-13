1832071a50de559e91773ae61d66f93c3dc76559
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-08-13 17:13:05 +0200

    Tweak the example app (#34)

--- example/src/components/Toolbar.js
+++ example/src/components/Toolbar.js
@@ -1,19 +1,21 @@
+/* @flow */
+
 import React from 'react';
 import { withMaterialPalette } from 'react-native-material-palette';
 import { Toolbar as ToolbarPaper } from 'react-native-paper';
 
-function ToolBar({ style, dark, onArrowBackPressed }) {
+function ToolBar({ style, palette, dark, onArrowBackPressed }) {
   return (
-    <ToolbarPaper style={style}>
-      <ToolbarPaper.Action
-        icon="keyboard-backspace"
-        onPress={onArrowBackPressed}
+    <ToolbarPaper dark={dark} style={style}>
+      <ToolbarPaper.Action icon="arrow-back" onPress={onArrowBackPressed} />
+      <ToolbarPaper.Content
+        title="Screen Details"
+        titleStyle={{ color: palette.muted.titleTextColor }}
       />
-      <ToolbarPaper.Content title="Screen Details" dark={dark} />
     </ToolbarPaper>
   );
 }
 
 export default withMaterialPalette(palette => ({
-  backgroundColor: palette.lightVibrant.color,
+  backgroundColor: palette.muted.color,
 }))(ToolBar);
